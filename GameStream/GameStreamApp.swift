f7298f9d354575487761996549d5f070ecf4375d
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-03-25 20:27:16 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Victor Hugo on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
